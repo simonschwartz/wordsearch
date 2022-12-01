71ea829fce912a95035f90dbba1630e9076849b5
Sasha Yee <yee.sasha@abc.net.au>
2022-12-02 10:50:52 +1100

    medium solution

--- js/index.js
+++ js/index.js
@@ -22,6 +22,36 @@ function wordSearch(puzzle, ...words) {
         }
     });
 
+    if (results.length === words.length) {
+        return results;
+    }
+
+    const cols = [];
+
+
+    // flipping the orientation of the array is hard.
+    for(let col = 0; col < puzzle[0].length; col++) {
+        let colVal = [];
+        for (let row = 0; row < puzzle.length; row++) {
+            colVal.push(puzzle[row][col]);
+        }
+
+        cols.push(colVal.join(''));
+    }
+
+    words.forEach(function (word) {
+        for (let i = 0; i < cols.length; i++) {
+            const index = cols[i].indexOf(word);
+            if (index >= 0) {
+                results[word] = [
+                    [index, i],
+                    [index + word.length - 1, i]
+                ];
+                break;
+            }
+        }
+    });
+
   return results;
 }
 
@@ -71,4 +101,24 @@ if (
   console.log("ERROR: Easy Puzzle failed");
   console.log("Expected:", expects.easyPuzzleExpected);
   console.log("Received:", easyPuzzleResult);
+}
+
+const mediumPuzzleResult = wordSearch(
+  MEDIUM_WORD_PUZZLE,
+  "humpty",
+  "bingo",
+  "bluey"
+);
+
+if (
+  isEqual(
+    wordSearch(MEDIUM_WORD_PUZZLE, "humpty", "bingo", "bluey"),
+    expects.mediumPuzzleExpected
+  )
+) {
+  console.log("SUCCESS: Medium puzzle solved");
+} else {
+  console.log("ERROR: Medium Puzzle failed");
+  console.log("Expected:", expects.mediumPuzzleExpected);
+  console.log("Received:", mediumPuzzleResult);
 }
