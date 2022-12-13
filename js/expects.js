27878627d877e400767f815925a52c5ed4ec7f61
nicholas.gledhill <gledhill.nicholas@abc.net.au>
2022-12-14 00:15:01 +1100

    jslint and more tidy up

--- js/expects.js
+++ js/expects.js
@@ -1,58 +1,58 @@
 module.exports = {
-  easyPuzzleExpected: {
-    humpty: [
-      [2, 1],
-      [2, 6],
-    ],
-    bluey: [
-      [4, 3],
-      [4, 7],
-    ],
-    bingo: [
-      [1, 3],
-      [1, 7],
-    ],
-  },
-  mediumPuzzleExpected: {
-    humpty: [
-      [0, 8],
-      [5, 8],
-    ],
-    bluey: [
-      [4, 3],
-      [4, 7],
-    ],
-    bingo: [
-      [0, 1],
-      [4, 1],
-    ],
-  },
-  hardPuzzleExpected: {
-    humpty: [
-      [0, 8],
-      [5, 8],
-    ],
-    bluey: [
-      [4, 3],
-      [4, 7],
-    ],
-    bingo: [
-      [5, 1],
-      [1, 5],
-    ],
-  },
-  extraHardPuzzleExpected: {
-    humpty: [
-      [5, 8],
-      [0, 8],
-    ],
-    bluey: [
-      [4, 7],
-      [4, 3],
-    ],
-    bingo: [
-      [1, 5],
-      [5, 1],
-    ],
-  },
+    easyPuzzleExpected: {
+        bingo: [
+            [1, 3],
+            [1, 7]
+        ],
+        bluey: [
+            [4, 3],
+            [4, 7]
+        ],
+        humpty: [
+            [2, 1],
+            [2, 6]
+        ]
+    },
+    extraHardPuzzleExpected: {
+        bingo: [
+            [1, 5],
+            [5, 1]
+        ],
+        bluey: [
+            [4, 7],
+            [4, 3]
+        ],
+        humpty: [
+            [5, 8],
+            [0, 8]
+        ]
+    },
+    hardPuzzleExpected: {
+        bingo: [
+            [5, 1],
+            [1, 5]
+        ],
+        bluey: [
+            [4, 3],
+            [4, 7]
+        ],
+        humpty: [
+            [0, 8],
+            [5, 8]
+        ]
+    },
+    mediumPuzzleExpected: {
+        bingo: [
+            [0, 1],
+            [4, 1]
+        ],
+        bluey: [
+            [4, 3],
+            [4, 7]
+        ],
+        humpty: [
+            [0, 8],
+            [5, 8]
+        ]
+    }
 };
