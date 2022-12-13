8bc9b9fc1582657805ed411fbe0726f4a96c9122
nicholas.gledhill <gledhill.nicholas@abc.net.au>
2022-12-14 00:16:53 +1100

    undoing some jslint for readability

--- js/expects.js
+++ js/expects.js
@@ -13,34 +13,6 @@ module.exports = {
             [2, 6]
         ]
     },
-    extraHardPuzzleExpected: {
-        bingo: [
-            [1, 5],
-            [5, 1]
-        ],
-        bluey: [
-            [4, 7],
-            [4, 3]
-        ],
-        humpty: [
-            [5, 8],
-            [0, 8]
-        ]
-    },
-    hardPuzzleExpected: {
-        bingo: [
-            [5, 1],
-            [1, 5]
-        ],
-        bluey: [
-            [4, 3],
-            [4, 7]
-        ],
-        humpty: [
-            [0, 8],
-            [5, 8]
-        ]
-    },
     mediumPuzzleExpected: {
         bingo: [
             [0, 1],
@@ -53,6 +25,34 @@ module.exports = {
         humpty: [
             [0, 8],
             [5, 8]
+        ]
+    },
+    hardPuzzleExpected: {
+      bingo: [
+          [5, 1],
+          [1, 5]
+      ],
+      bluey: [
+          [4, 3],
+          [4, 7]
+      ],
+      humpty: [
+          [0, 8],
+          [5, 8]
+      ]
+    },
+    extraHardPuzzleExpected: {
+        bingo: [
+            [1, 5],
+            [5, 1]
+        ],
+        bluey: [
+            [4, 7],
+            [4, 3]
+        ],
+        humpty: [
+            [5, 8],
+            [0, 8]
         ]
     }
 };
