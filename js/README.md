f5bfa17826b11a692ff0f291d7eb1c0ca719d2e1
Gillian Pan <pan.gillian@abc.net.au>
2022-12-15 10:59:45 +1100

    Add readme, tidyup

--- js/README.md
+++ js/README.md
@@ -13,3 +13,39 @@ Update `index.js` and run
 ```
 npm run start
 ```
+
+# How does it do
+
+## Horizontal
+
+- Horizontal word search is easy using JS's array manipulation functions.
+- First, flatten the 2D array into a string.
+- Then do an Array.indexOf to find the target word.
+
+## Vertical
+
+- This is 90% the same as the horizontal word search
+- Except that before doing the Array.indexOf, the array was rotated 90 degrees clockwise using Array splicing.
+
+## Diagonal
+
+- Idea 1 - shift + rotate array
+- Idea 2 -
+  - Created array of all diagonals -
+   - [ 00,             ->
+      10 01           ->
+      20 11 02        ->
+      30 21 12 03     ->
+      40 31 22 13 04  ->
+      50 41 32 23 14  ->
+      51 42 33 24     ->
+      52 43 34        ->
+      53 35           ->
+      55              ->
+    ]
+   - Unfinished.
+
+# Limitations
+- Heavy reliance on Array.indexOf function.
+- Search directions is limited to W-E (horizontal), N-S (vertical)
+-
--- js/index.js
+++ js/index.js
@@ -14,21 +14,51 @@ function wordSearch(puzzle, ...words) {
   expected = {}
   words.forEach(word => expected[word] = null)
 
+  // Each puzzle relied on popuating a flattened string and then
+  // doing an Array.indexOf over the string.
+
   // horizontal - easy
-  // - Flatten array
-  // - Word search (flattenedArray.indexOf(targetWord))
   const flattenedArrHorizontal = puzzle.flat().join('');
 
   // vertical - medium
-  // - Rotate the array 90 degrees clockwise
-  // - Flatten array.
-  // - Word search (flattenedArray.indexOf(targetWord))
   let puzzleRotated = []
   const puzzleCopy = puzzle.map(x => x.slice());
   for (let i = 0; i < xLen; i++){
     puzzleRotated = puzzleRotated.concat(puzzleCopy.map(row => row.shift()))
   }
   flattenedArrVertical = puzzleRotated.flat().join('')
+
+  // // diagonal
+  // let diagonalCoordArray = []
+  // let flattenedArrDiagonal = '';
+  // for (let i = 0; i< yLen-1; i++){
+  //   let xId = i
+  //   let yId = 0
+  //   let tmpArr = []
+  //   while (xId >= 0){
+  //     diagonalCoordArray.push([xId,yId])
+  //     tmpArr.push([xId,yId])
+  //     flattenedArrDiagonal += puzzle[xId,yId]
+  //     xId = xId - 1
+  //     yId = yId + 1
+  //   }
+  //   console.log(tmpArr)
+  // }
+  // console.log('-')
+  // for (let i = 0; i< yLen-1; i++){
+  //   let xId = yLen-i-1;
+  //   let yId = i
+  //   let tmpArr = [];
+
+  //   while (xId > 0){
+  //     diagonalCoordArray.push([yLen-1, yId])
+  //     tmpArr.push([yLen-1, yId])
+  //     flattenedArrDiagonal += puzzle[yLen-1, yId]
+  //     xId = xId - 1;
+  //     yId = yId + 1
+  //   }
+  //   console.log(tmpArr)
+  // }
 
   // find words - for the easy + med puzzle
   words.forEach(word => {
@@ -50,9 +80,6 @@ function wordSearch(puzzle, ...words) {
       expected[word] = [[col, row], [col + word.length -1 , row ]]
     }
   })
-
-  // find words - for the hard puzzle.
-  // - ss
 
   return expected;
 }
@@ -76,19 +103,18 @@ const MEDIUM_WORD_PUZZLE = [
 ];
 
 const HARD_WORD_PUZZLE = [
-  ["h", "b", "i", "n", "e", "a", "b", "c", "h", "e"], // shift -5, row.slice(5).concat(row.slice(0,5))
-  ["a", "i", "c", "b", "i", "o", "z", "p", "u", "e"], // shift -4, row.slice(4).concat(row.slice(0,4))
-  ["a", "g", "u", "m", "g", "t", "y", "o", "m", "e"], // shift -3, row.slice(3).concat(row.slice(0,3))
-  ["h", "g", "i", "n", "e", "a", "b", "c", "p", "e"], // shift -2, row.slice(2).concat(row.slice(0,2))
-  ["h", "o", "i", "b", "l", "u", "e", "y", "t", "e"], // shift -1, row.slice(1).concat(row.slice(0,1))
+  ["h", "b", "i", "n", "e", "a", "b", "c", "h", "e"],
+  ["a", "i", "c", "b", "i", "o", "z", "p", "u", "e"],
+  ["a", "g", "u", "m", "g", "t", "y", "o", "m", "e"],
+  ["h", "g", "i", "n", "e", "a", "b", "c", "p", "e"],
+  ["h", "o", "i", "b", "l", "u", "e", "y", "t", "e"],
   ["h", "b", "i", "b", "l", "u", "o", "z", "y", "e"],
-// sh-0, sh-1, sh2, sh-3...
 ];
 
 // Test cases
 const testcases = [
-  // { name: 'easy', tc: EASY_WORD_PUZZLE, expected: expects.easyPuzzleExpected, got: null },
-  // { name: 'medium', tc: MEDIUM_WORD_PUZZLE, expected: expects.mediumPuzzleExpected, got: null },
+  { name: 'easy', tc: EASY_WORD_PUZZLE, expected: expects.easyPuzzleExpected, got: null },
+  { name: 'medium', tc: MEDIUM_WORD_PUZZLE, expected: expects.mediumPuzzleExpected, got: null },
   { name: 'hard', tc: HARD_WORD_PUZZLE, expected: expects.hardPuzzleExpected, got: null }
 ]
 testcases.forEach((tc) => {
