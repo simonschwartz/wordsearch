7a0c264a1730f18c657541b85a1c1d9a8f03773a
Simon Schwartz <schwartz.simon@abc.net.au>
2022-12-14 20:54:53 +1100

    bump

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-
 type WordPuzzle = [[char; 10]; 6];
 
 const EASY_WORD_PUZZLE: WordPuzzle = [
@@ -20,73 +18,93 @@ struct SearchResults {
     humpty: SearchResult,
 }
 
-type SurroundCoords = [[usize; 2]; 1];
-
-fn walk(start: [usize; 2], end: [usize; 2], word: &str) {
-    // dont need to do first or last
-    let char_vec: Vec<char> = word.chars().collect();
-    for c in char_vec {
-        println!("{}", c);
+fn walk(start: [usize; 2], end: [usize; 2], amputated: &str) -> SearchResult {
+    let char_vec: Vec<char> = amputated.chars().collect();
+    for (i, c) in char_vec.iter().enumerate() {
+        let start_row = start[0];
+        let start_col = start[1];
+        if EASY_WORD_PUZZLE[start_row][start_col + 1 + i] == *c {
+            continue;
+        }
+        return [[0, 0], [0, 0]];
     }
+
+    return [start, end];
 }
 
 // 1. Walk the array to see if first letter of word exists
 // 1. If we get a match then check if last letter of word exists in relevant position
 // 2. If it does - walk each letter between first and last to confirm a match
-fn check_surrounds(row: usize, col: usize, letter: char, length: usize) -> Result<SearchResult, dyn Error> {
-    let word_length = length - 1;
-    if col + word_length < 10 {
-        let left_to_right = [row, col + word_length];
-        let last_letter_maybe = EASY_WORD_PUZZLE[left_to_right[0]][left_to_right[1]];
-
-        if last_letter_maybe == letter {
-            println!(
-                "last_letter_maybe {:?} {} {}",
-                last_letter_maybe,
-                row,
-                col + word_length
+fn check_surrounds(
+    position: [usize; 2],
+    last_char: char,
+    amputated: &str,
+    word_length: usize,
+) -> SearchResult {
+    if position[1] + word_length < 10 {
+        let left_to_right = [position[0], position[1] + word_length - 1];
+        // let top_to_bottom = [position[0] + word_length - 1, position[1]];
+        let last_letter = EASY_WORD_PUZZLE[left_to_right[0]][left_to_right[1]];
+        if last_char == last_letter {
+            let result = walk(
+                [position[0], position[1]],
+                [position[0], position[1] + word_length - 1],
+                amputated,
             );
-            let result =   walk([row, col], [row, col + word_length], "bluey");
-
-            match result {
-                Ok(coords) => coords,
-                Err(e) => return Err(e),
-            }
-            // walk([row, col], [row, col + word_length], "bluey");
-            // return Some([[row, col], [row, col + word_length]]);
+            return result;
         }
     }
 
-    // return Some();
+    return [[0, 0], [0, 0]];
 }
 
-fn find_word(puzzle: WordPuzzle, word: &str) -> SearchResults {
-    let first_char = word.chars().nth(0).unwrap();
-    let last_char = word.chars().nth(0).unwrap();
-    let word_length = word.chars().count();
+fn check(row: usize, col: usize, words: Words, puzzle: WordPuzzle) -> SearchResult {
+    for word in words {
+        let first_char = word.chars().nth(0).unwrap();
+        if first_char == puzzle[row][col] {
+            println!("Match {}", word);
+        }
+    }
 
+    return [[0, 0], [0, 0]];
+}
+
+type Words<'a> = [&'a str; 3];
+
+fn find_words(puzzle: WordPuzzle, word: &str) -> SearchResult {
+    // works
+    let first_char = word.chars().nth(0).unwrap();
+    let last_char = word.chars().last().unwrap();
+    let word_length = word.len();
+    let amputated: &str = &word[1..word_length - 1];
+
+    // let mut words: Words = [
+    //     "bluey",
+    //     "bingo",
+    //     "humpty"
+    // ];
+
+    // scan the puzzle for letters that match the first character of the word we are searching for
     for row in 0..puzzle.len() {
         for col in 0..puzzle[row].len() {
             let letter = puzzle[row][col];
-            // println!("{} {} {}", puzzle[i][n], row, col);
+
+            // let result = check(row, col, words, puzzle);
+            // works
             if letter == first_char {
-                // println!("rowsss");
-                let b: SearchResult = check_surrounds(row, col, last_char, word_length);
-                //println!("row {}", i);
-                //println!("col {}", n);
-                // println!("{}", puzzle[i][n]);
-                // println!("left {} {}", b[0][0], b[0][1]);
-                // break;
+                // When we find the first letter, scan its surrounds to see if we have a match
+                let result: SearchResult =
+                    check_surrounds([row, col], last_char, amputated, word_length);
+                if result == [[0, 0], [0, 0]] {
+                    continue;
+                } else {
+                    return result;
+                }
             }
         }
     }
 
-    let result = SearchResults {
-        bluey: [[2, 5], [1, 2]],
-        bingo: [[2, 5], [1, 2]],
-        humpty: [[2, 5], [1, 2]],
-    };
-    return result;
+    return [[0, 0], [0, 0]];
 }
 
 // const MEDIUM_WORD_PUZZLE: WordPuzzle = [
@@ -107,49 +125,60 @@ fn find_word(puzzle: WordPuzzle, word: &str) -> SearchResults {
 //     ['h', 'b', 'i', 'b', 'l', 'u', 'o', 'z', 'y', 'e'],
 // ];
 
-fn main() {
+fn main(puzzle: WordPuzzle) -> SearchResults {
     use std::time::Instant;
     let now = Instant::now();
-    find_word(EASY_WORD_PUZZLE, "bluey");
+    let bingo = find_words(puzzle, "bingo");
+    let bluey = find_words(puzzle, "bluey");
+    let humpty = find_words(puzzle, "humpty");
+    let result = {
+        SearchResults {
+            bluey: bluey,
+            bingo: bingo,
+            humpty: humpty,
+        }
+    };
+    println!("Result: {:?}", result);
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
+    return result;
 }
 
 #[test]
 fn word_search_test_easy() {
-    let result = word_search(EASY_WORD_PUZZLE);
+    let result = main(EASY_WORD_PUZZLE);
     assert_eq!(
         result,
-        SearchResult {
-            bluey: [[2, 5], [1, 3]],
-            bingo: [[2, 5], [1, 2]],
-            humpty: [[2, 5], [1, 2]],
+        SearchResults {
+            bluey: [[4, 3], [4, 7]],
+            bingo: [[1, 3], [1, 7]],
+            humpty: [[2, 1], [2, 6]],
         }
     );
 }
 
-#[test]
-fn word_search_test_medum() {
-    let result = word_search(MEDIUM_WORD_PUZZLE);
-    assert_eq!(
-        result,
-        SearchResult {
-            bluey: [[2, 5], [1, 3]],
-            bingo: [[2, 5], [1, 2]],
-            humpty: [[2, 5], [1, 2]],
-        }
-    );
-}
+// #[test]
+// fn word_search_test_medum() {
+//     let result = word_search(MEDIUM_WORD_PUZZLE);
+//     assert_eq!(
+//         result,
+//         SearchResult {
+//             bluey: [[2, 5], [1, 3]],
+//             bingo: [[2, 5], [1, 2]],
+//             humpty: [[2, 5], [1, 2]],
+//         }
+//     );
+// }
 
-#[test]
-fn word_search_test_hard() {
-    let result = word_search(HARD_WORD_PUZZLE);
-    assert_eq!(
-        result,
-        SearchResult {
-            bluey: [[2, 5], [1, 3]],
-            bingo: [[2, 5], [1, 2]],
-            humpty: [[2, 5], [1, 2]],
-        }
-    );
-}
+// #[test]
+// fn word_search_test_hard() {
+//     let result = word_search(HARD_WORD_PUZZLE);
+//     assert_eq!(
+//         result,
+//         SearchResult {
+//             bluey: [[2, 5], [1, 3]],
+//             bingo: [[2, 5], [1, 2]],
+//             humpty: [[2, 5], [1, 2]],
+//         }
+//     );
+// }
